dff9233c64c17e1acd8abbb24f42401a89c332cb
AshwinC8 <ashwinsam01@yahoo.com>
2021-11-18 11:26:31 +0530

    first commit

---
+++ Dlists.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#This file should be sourced
+
+filename='/mnt/d/ashwi/College/tools/DL.txt';
+
+function append() {
+  if [ "$1" != "" ]; then
+      echo $1 >> $filename;
+  fi
+}
+
+function delete() {
+  echo -e "    \e[1;4mBOOKMARKS\e[0m\n";
+
+  local count=1;
+  for i in $(cat $filename); do
+    echo "$count. $i";
+    count=$(($count+1));
+  done
+  wait
+
+  echo -e -n "\nEnter choice: ";
+  read count;
+  
+  sed -i ''$count''d'' $filename
+}
+
+function BookmarksLists(){
+  echo -e "    \e[1;4mBOOKMARKS\e[0m\n";
+
+  local count=1;
+  for i in $(cat $filename); do
+    echo "$count. $i";
+    count=$(($count+1));
+  done
+  wait
+
+  echo -e -n "\nEnter choice: ";
+  read count;
+
+  var=$(sed -n ${count}p $filename);
+  cd $var;
+}
+
+
+
+if [[ "$1" == "add" ]] then
+  append $2;
+elif [[ "$1" == "delete" ]] then
+  delete ;
+else
+  BookmarksLists;
+fi
+
+
